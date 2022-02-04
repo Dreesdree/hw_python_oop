13cf39a3c91fe278dea08e0f4f0d38c87a87725e
Денис Иванов <deni_95@list.ru>
2022-02-04 13:18:23 +0500

    correct task angain

--- homework.py
+++ homework.py
@@ -112,8 +112,8 @@ class Swimming(Training):
                 / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
-        return (self.get_mean_speed() + self.COEF_CALORIE_1) \
-               * 2 * self.weight
+        return ((self.get_mean_speed() + self.COEF_CALORIE_1)
+                * 2 * self.weight)
 
 
 Trainings: dict = {'RUN': Running, 'WLK': SportsWalking, 'SWM': Swimming}
